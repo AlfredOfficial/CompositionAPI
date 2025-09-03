5bdeace7809601752c2e5f85d03af03a52493a7b
AlfredOfficial <orapaalfred7@gmail.com>
2025-09-03 19:38:59 +0800

    add css for nav

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,28 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
     <div class="wrapper">
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/" class="nav-link">Home</RouterLink>
       </nav>
     </div>
   </header>
 
   <RouterView />
 </template>
+
+<style lang="scss" scoped>
+nav {
+  padding: 1rem 2rem;
+  background-color: #90caf9;
+  border-bottom: 1px solid #90caf9;
+  .nav-link {
+    color: #fff;
+    padding: 5px 15px;
+    border-radius: 5px;
+    transition: all 0.2s ease-in-out;
+    margin-right: 1rem;
+    &:hover {
+      background-color: #90caf9;
+    }
+  }
+}
+</style>
